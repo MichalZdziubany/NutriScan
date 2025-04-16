2b5bff0a41616d804f7271b50f79b32c756fac26
MichalZdziubany <127780743+MichalZdziubany@users.noreply.github.com>
2025-04-17 00:06:53 +0100

    changed pwa approach to native android

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'nutri-scan',
-  webDir: 'www'
-};
-
-export default config;
--- src/app/pages/scan/scan.page.ts
+++ src/app/pages/scan/scan.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
+
 @Component({
   selector: 'app-scan',
   templateUrl: './scan.page.html',
