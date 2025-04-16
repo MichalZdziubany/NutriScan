29151b04a06aa242bb3cd484ac70f8558f43dd65
MichalZdziubany <127780743+MichalZdziubany@users.noreply.github.com>
2025-04-16 15:24:10 +0100

    fixed routing, added a starting log in page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton ,IonItem, IonLabel} from '@ionic/angular/standalone';
+import { Router ,RouterModule} from '@angular/router'; // Import Angular Router
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterModule
+            , IonButton, IonItem, IonLabel]
 })
 export class LoginPage implements OnInit {
+  
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
-  constructor() { }
-
+  constructor(private router: Router) {}
+  
   ngOnInit() {
+    //if user is logged in then redirect to home page
+    // Placeholder: Check if user is logged in (Firebase will be added later)
   }
 
+  // Login method to be implemented when Firebase is added later
+  async login() {
+    // Placeholder: Firebase will be added here later for login functionality
+    console.log('Email:', this.email, 'Password:', this.password);
+    // For now, assume login is successful and navigate to the home page
+    this.router.navigate(['/home']);
+  }
 }
--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar , IonButton} from '@ionic/angular/standalone';
+import { Router ,RouterModule} from '@angular/router'; // Import Angular Router
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, RouterModule]
 })
-export class WelcomePage implements OnInit {
+export class WelcomePage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  navigateToLogin() {
+    this.router.navigate(['/login']);
   }
-
+  navigateToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
